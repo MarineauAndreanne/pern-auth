4e2a0e30b21dc9236bdaab6f4242c7439e0048e4
MarineauAndreanne <83275409+MarineauAndreanne@users.noreply.github.com>
2022-06-10 11:32:39 -0400

    backend

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+const { config } = require("dotenv")
+config()
+
+module.exports = {
+  PORT: process.env.PORT,
+  SECRET: process.env.SECRET,
+  CLIENT_URL: process.env.CLIENT_URL,
+  SERVER_URL: process.env.SECRET_URL,
+}
